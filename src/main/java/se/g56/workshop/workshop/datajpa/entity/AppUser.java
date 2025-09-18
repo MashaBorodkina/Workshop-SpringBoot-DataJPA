a617c41f79f0874677ae80b81c5136e3e085cee8
Maria Borodkina <mashaborodkina1507@ya.ru>
2025-09-18 13:32:18 +0300

    feat(appuser): add AppUser entity with identity id and unique username

---
+++ src/main/java/se/g56/workshop/workshop/datajpa/entity/AppUser.java
@@ -0,0 +1,50 @@
+package se.g56.workshop.workshop.datajpa.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@ToString (exclude = {"password", "details"})
+@NoArgsConstructor(access = PROTECTED)
+@RequiredArgsConstructor
+
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    @Setter
+    @NotBlank
+    @NonNull
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    @Setter
+    @NotBlank
+    @NonNull
+    private String password;
+
+    @Column(nullable = false)
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime regDate;
+
+    @OneToOne (cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(nullable = false, unique = true, name="details_id")
+    @NonNull
+    Details userDetails;
+
+    @PrePersist
+    private void onCreate(){
+        this.regDate = LocalDateTime.now();
+    }
+
+}

f2079e78b05c1f0d67c532afa285cea61153f0ef
Maria Borodkina <mashaborodkina1507@ya.ru>
2025-09-18 11:05:37 +0300

    chore: init Spring Boot project

---
+++ src/test/java/se/g56/workshop/workshop/datajpa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.g56.workshop.workshop.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b3f86d63165ce78dbd62fd1547007fcaf939e106
Maria Borodkina <mashaborodkina1507@ya.ru>
2025-09-19 15:57:17 +0300

    feat(repo-Details): add methods (findByBirthDateBetween, findByMonth)

--- src/main/java/se/g56/workshop/workshop/datajpa/repo/DetailsRepository.java
+++ src/main/java/se/g56/workshop/workshop/datajpa/repo/DetailsRepository.java
@@ -1,8 +1,11 @@
 package se.g56.workshop.workshop.datajpa.repo;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import se.g56.workshop.workshop.datajpa.entity.Details;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +14,8 @@ public interface DetailsRepository extends CrudRepository<Details, Long> {
     Optional<Details> findByEmail(String email);
     List<Details> findByNameContainingIgnoreCase(String name);
     List<Details> findByNameIgnoreCase(String name);
+    List<Details> findByBirthDateBetween(LocalDate start, LocalDate end);
+
+    @Query("SELECT d FROM Details d WHERE MONTH(d.birthDate)=:month")
+    List<Details> findByMonth(@Param("month") int month);
 }

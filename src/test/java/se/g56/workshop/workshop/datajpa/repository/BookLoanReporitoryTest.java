06aed97983765ed56ecb6f42d80c1a456b1006d4
Maria Borodkina <mashaborodkina1507@ya.ru>
2025-09-29 13:02:05 +0300

    test(book-loan-repo): add @DataJpaTest for BookLoanRepository -findByBorrowerId -findByBookId -findByReturnedFalse -findByReturnedFalseAndDueDateBefore -findAllByLoanDateBetween -markReturnedById

--- src/test/java/se/g56/workshop/workshop/datajpa/repository/BookLoanReporitoryTest.java
+++ src/test/java/se/g56/workshop/workshop/datajpa/repository/BookLoanReporitoryTest.java
@@ -30,16 +30,128 @@ public class BookLoanReporitoryTest {
     @Test
     @DisplayName("Find by borrower ID")
     void findByBorrowerID() {
-        Details details =  new Details("email@email.se", "Name", LocalDate.of(1990, 1, 1));
+        Details details1 =  new Details("email@email.se", "Name", LocalDate.of(1990, 1, 1));
+        Book book1 = bookRepository.save(new Book("isbn12345678", "title", 10));
+        AppUser user1 = appUserRepository.save(new AppUser("un", "password",details1));
+
+
+        Book book2 = bookRepository.save(new Book("isbn012345679", "title2", 10));
+
+
+        BookLoan l1 = bookLoanRepository.save(BookLoan.issue(user1, book1, LocalDate.of(1990, 1, 1)));
+        BookLoan l2 = bookLoanRepository.save(BookLoan.issue(user1, book2, LocalDate.of(1990, 1, 1)));
+
+        var result = bookLoanRepository.findByBorrowerId(l1.getBorrower().getId());
+
+        assertThat(result)
+                .extracting(BookLoan::getId)
+                .containsExactlyInAnyOrder(l1.getId(), l2.getId());
+    }
+
+    @Test
+    @DisplayName("Find by book ID")
+    void findByBookID() {
+        Details details = new Details("email@email.se", "Name", LocalDate.of(1990, 1, 1));
         Book book = bookRepository.save(new Book("isbn12345678", "title", 10));
         AppUser user = appUserRepository.save(new AppUser("un", "password",details));
 
         BookLoan l = bookLoanRepository.save(BookLoan.issue(user, book, LocalDate.of(1990, 1, 1)));
 
-        var result = bookLoanRepository.findByBorrowerId(l.getBorrower().getId());
+        var result = bookLoanRepository.findByBookId(l.getBook().getId());
 
         assertThat(result)
                 .extracting(BookLoan::getId)
                 .containsExactly(l.getId());
+    }
+
+    @Test
+    @DisplayName("Find by returned false")
+
+    void findByReturnedFalse() {
+        Details details1 = new Details("email@email.se", "Name", LocalDate.of(1990, 1, 1));
+        Book book1 = bookRepository.save(new Book("isbn12345678", "title", 10));
+        AppUser user1 = appUserRepository.save(new AppUser("un", "password", details1));
+
+        Book book2 = bookRepository.save(new Book("isbn012345679", "title2", 10));
+
+
+        BookLoan l1 = bookLoanRepository.save(BookLoan.issue(user1, book1, LocalDate.of(1990, 1, 1)));
+        BookLoan l2 = bookLoanRepository.save(BookLoan.issue(user1, book2, LocalDate.of(1990, 1, 1)));
+
+        l1.markReturned();
+        bookLoanRepository.flush();
+
+        var result = bookLoanRepository.findByReturnedFalse();
+
+        assertThat(result)
+                .size()
+                .isEqualTo(1);
+
+        assertThat(result)
+                .extracting(BookLoan::getId)
+                .containsExactly(l2.getId());
+    }
+    @Test
+    @DisplayName("Find by returned false and due date before date")
+    void findByReturnedFalseAndDueDateBefore(){
+        Details details1 = new Details("email@email.se", "Name", LocalDate.of(1990, 1, 1));
+        Book book1 = bookRepository.save(new Book("isbn12345678", "title", 10));
+        AppUser user1 = appUserRepository.save(new AppUser("un", "password", details1));
+
+        Book book2 = bookRepository.save(new Book("isbn012345679", "title2", 10));
+
+
+        BookLoan l1 = bookLoanRepository.save(BookLoan.issue(user1, book1,LocalDate.of(2025, 9, 18)));
+        BookLoan l2 = bookLoanRepository.save(BookLoan.issue(user1, book2, LocalDate.of(2025, 9, 25)));
+
+        var result = bookLoanRepository.findByReturnedFalseAndDueDateBefore(LocalDate.of(2025, 9, 29));
+
+        assertThat(result)
+                .extracting(BookLoan::getId)
+                .containsExactly(l1.getId());
+        assertThat(result)
+                .extracting(BookLoan::getId)
+                .size()
+                .isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("Find all by loan date between dates")
+    void findAllByLoanDateBetween(){
+        Details details1 = new Details("email@email.se", "Name", LocalDate.of(1990, 1, 1));
+        Book book1 = bookRepository.save(new Book("isbn12345678", "title", 10));
+        AppUser user1 = appUserRepository.save(new AppUser("un", "password", details1));
+
+        Book book2 = bookRepository.save(new Book("isbn012345679", "title2", 10));
+
+
+        BookLoan l1 = bookLoanRepository.save(BookLoan.issue(user1, book1,LocalDate.of(2025, 9, 18)));
+        BookLoan l2 = bookLoanRepository.save(BookLoan.issue(user1, book2, LocalDate.of(2025, 9, 25)));
+
+        var result = bookLoanRepository.findAllByLoanDateBetween(LocalDate.of(2025, 9, 1), LocalDate.of(2025, 9, 29));
+
+        assertThat(result)
+                .hasSize(2);
+        assertThat(result)
+                .extracting(BookLoan::getId)
+                .containsExactlyInAnyOrder(l1.getId(), l2.getId());
+    }
+
+    @Test
+    @DisplayName("mark returned by Id")
+    void markReturnedById(){
+        Details details1 = new Details("email@email.se", "Name", LocalDate.of(1990, 1, 1));
+        Book book1 = bookRepository.save(new Book("isbn12345678", "title", 10));
+        AppUser user1 = appUserRepository.save(new AppUser("un", "password", details1));
+
+        Book book2 = bookRepository.save(new Book("isbn012345679", "title2", 10));
+
+
+        BookLoan l1 = bookLoanRepository.save(BookLoan.issue(user1, book1,LocalDate.of(2025, 9, 18)));
+        BookLoan l2 = bookLoanRepository.save(BookLoan.issue(user1, book2, LocalDate.of(2025, 9, 25)));
+
+        assertThat(bookLoanRepository.markReturnedById(l1.getId()))
+                .isEqualTo(1);
+        assertThat(bookLoanRepository.findByReturnedFalse()).doesNotContain(l1);
     }
 }

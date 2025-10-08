4dc62e4618396eede820e78d5d81ee4b69705dd4
Maria Borodkina <mashaborodkina1507@ya.ru>
2025-10-08 12:03:47 +0300

    feat(details): add DetailsService interface with createDetails method

---
+++ src/main/java/se/g56/workshop/workshop/datajpa/service/DetailsService.java
@@ -0,0 +1,7 @@
+package se.g56.workshop.workshop.datajpa.service;
+
+import se.g56.workshop.workshop.datajpa.entity.Details;
+
+public interface DetailsService {
+    Details createDetails(Details details);
+}
